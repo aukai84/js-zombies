e8816fdaef794c57dc86207c509e9003b3d247a7
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-08 21:42:12 -1000

    created test to check if Weapon is an instance of Item

--- test/test-script.js
+++ test/test-script.js
@@ -69,6 +69,9 @@ describe("Food", () => {
   it("should have a name", () => {
     pizza.name.should.equal("Pizza")
   });
+  it("should be an instance of Item", () => {
+    pizza.should.be.an.instanceof(Item)
+  });
 });
 
 
