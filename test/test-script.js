73bedfb529533210790fb09a3086674e7163cb4d
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-08 20:29:53 -1000

    test if class working

---
+++ test/test-script.js
@@ -0,0 +1,12 @@
+/*jshint esversion: 6 */
+const chai = require('chai');
+const Zombies = require('../zombies');
+chai.should();
+
+describe("Item", () => {
+  let item = Zombies.item;
+
+  it("should be a class", () => {
+    item.should.be.a.function
+  })
+});;
