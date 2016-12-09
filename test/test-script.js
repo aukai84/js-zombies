bfa86d00f824bc1d04bba51f2aff743c6d58111f
Aukai Tirrell <mtirrell@hawaii.edu>
2016-12-08 20:37:38 -1000

    created test for having a parameter

--- test/test-script.js
+++ test/test-script.js
@@ -4,9 +4,15 @@ const Zombies = require('../zombies');
 chai.should();
 
 describe("Item", () => {
+
   let item = Zombies.item;
 
   it("should be a class", () => {
     item.should.be.a.function
+  });
+
+  it("should have one parameter", () => {
+    let stick = new item("stick");
+    stick.name.should.equal("stick");
   })
 });;
